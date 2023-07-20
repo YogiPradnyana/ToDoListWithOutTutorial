7940d3074995eba5946408fa65ddb332c694a8b5
YogiPradnyana <15tkj1yogipradnyana.com>
2023-07-20 22:23:50 +0800

    Mengubah file

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
 const todo = document.getElementById('todo');
 
+function showAlert(message, className) {
+  const div = document.createElement('div');
+  div.className(`alert alert-${className}`);
+  div.appendChild(document.createTextNode(message));
+
+  const container = document.querySelectorAll('.container')[1];
+  const form = document.querySelector('.form-todo');
+  container.insertBefore(div, form);
+}
+
 function add() {
   // 1. Mengambil nilai pada input
   let newText = document.getElementById('new_text');
 
-  // 2. Menambahkan li baru
-  let newTodo = '<li><span onclick="beriGaris(this)">' + newText.value + '</span>' + '<span onclick="removeBtn(this)">[x]</span>' + '</li>';
-  todo.insertAdjacentHTML('afterbegin', newTodo);
-
-  // 3. Menghapus isi fieldnya
-  newText.value = '';
+  // Validated
+  if (newText.value == '') {
+    showAlert('Input kosong tidak diterima', 'danger');
+  } else {
+    // 2. Menambahkan li baru
+    let newTodo = '<li><span onclick="beriGaris(this)">' + newText.value + '</span>' + '<span onclick="removeBtn(this)">[x]</span>' + '</li>';
+    todo.insertAdjacentHTML('afterbegin', newTodo);
+    // 3. Menghapus isi fieldnya
+    newText.value = '';
+  }
 }
 
 function beriGaris(el) {
