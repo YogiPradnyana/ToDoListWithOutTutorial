3bf85a8467928461216b51dcf1caed92cd0ca15f
YogiPradnyana <15tkj1yogipradnyana.com>
2023-07-21 10:15:07 +0800

    Mengupdate list

--- script.js
+++ script.js
@@ -2,12 +2,13 @@ const todo = document.getElementById('todo');
 
 function showAlert(message, className) {
   const div = document.createElement('div');
-  div.className = `alert alert-${className}`;
+  div.className = `alert alert-${className} mt-2 col-5`;
   div.appendChild(document.createTextNode(message));
 
   const container = document.querySelectorAll('.container')[1];
-  const form = document.querySelector('.form-todo');
-  container.insertBefore(div, form);
+  // const form = document.querySelector('.form-todo');
+  const ul = document.getElementById('todo');
+  container.insertBefore(div, ul);
 
   setTimeout(() => {
     document.querySelector('.alert').remove();
@@ -23,7 +24,11 @@ function add() {
     showAlert('Input kosong tidak diterima', 'danger');
   } else {
     // 2. Menambahkan li baru
-    let newTodo = '<li><span onclick="beriGaris(this)">' + newText.value + '</span>' + '<span onclick="removeBtn(this)">[x]</span>' + '</li>';
+    let newTodo = `<li style="margin-left: -32px;">
+    <span>${newText.value}</span>
+    <span onclick="beriGaris(this)">[v]</span>
+    <span onclick="removeBtn(this)">[x]</span>
+    </li>`;
     todo.insertAdjacentHTML('afterbegin', newTodo);
     // 3. Menghapus isi fieldnya
     newText.value = '';
@@ -31,7 +36,7 @@ function add() {
 }
 
 function beriGaris(el) {
-  el.classList.toggle('done');
+  el.previousElementSibling.classList.toggle('done');
 }
 
 function removeBtn(el) {
